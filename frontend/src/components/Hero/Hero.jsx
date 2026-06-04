37c269c64b5bfd29e915e10905873a9892e6f215
Kevin-13579 <kevinlokesh333@gmail.com>
2026-06-04 22:48:55 +0530

    linkedin  connection

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = ({ theme }) => {
               
               {/* Social Accounts Sidebar stacked vertically as seen in image_30cf9d.jpg */}
               <div className="dark-social-vertical-bar">
-                <a href="https://linkedin.com/in/kevin-p" target="_blank" rel="noreferrer" className="social-icon-link">
+                <a href="https://www.linkedin.com/in/kevin-p-873731321?utm_source=share_via&utm_content=profile&utm_medium=member_android" target="_blank" rel="noreferrer" className="social-icon-link">
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zM7.119 20.452H3.554V9h3.565v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
                 </a>
                 <a href="https://github.com/Kevin-13579" target="_blank" rel="noreferrer" className="social-icon-link">
@@ -52,7 +52,7 @@ I build scalable, secure, multi-role web applications from the ground up. Specia
                 <a href="#projects" className="btn-teal-fill">LEARN MORE</a>
                 <a href="#contact" className="btn-dark-pill">HIRE ME</a>
                 <div className="light-social-icons">
-                  <a href="https://linkedin.com/in/kevin-p" target="_blank" rel="noreferrer" className="social-icon-btn">
+                  <a href="https://www.linkedin.com/in/kevin-p-873731321?utm_source=share_via&utm_content=profile&utm_medium=member_android" target="_blank" rel="noreferrer" className="social-icon-btn">
                     <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zM7.119 20.452H3.554V9h3.565v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
                   </a>
                   <a href="https://github.com/Kevin-13579" target="_blank" rel="noreferrer" className="social-icon-btn">
